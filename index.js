c2faf717483bfaefb76ce360ea1a22b969d44da1
Al Cerdan Lico <alcerdan@yahoo.com>
2024-09-19 11:55:09 -0700

    completed domcontentloaded

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let e = document.getElementById('text');
+    e.textContent = "This is really cool!";
+  });
+  
